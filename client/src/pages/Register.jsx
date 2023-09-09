66745c37ccab71751095ce1bc562c64fdb472d8e
Manas Nikte <manas.nikte@gmail.com>
2023-09-09 14:16:44 +0530

    eighth commit

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,19 +1,25 @@
 import { Logo, FormRow } from '../components';
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
 import { Form, redirect, useNavigation, Link } from 'react-router-dom';
+import customFetch from '../utils/customFetch';
+import { toast } from 'react-toastify';
 
 export const action = async ({ request }) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
   try {
     await customFetch.post('/auth/register', data);
+    toast.success('Registration successful');
     return redirect('/login');
   } catch (error) {
+    toast.error(error?.response?.data?.msg);
     return error;
   }
 };
 
 const Register = () => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
   return (
     <Wrapper>
       <Form method="post" className='form'>
@@ -26,8 +32,8 @@ const Register = () => {
 
         <FormRow type='password' name='password' />
 
-        <button type='submit' className='btn btn-block'>
-          submit
+        <button type='submit' className='btn btn-block' disabled={isSubmitting}>
+          {isSubmitting ? 'submitting...' : 'submit'}
         </button>
         <p>
           Already a member?

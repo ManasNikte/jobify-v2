9d31d9d305c218f4625e52978a9a34313b27f4cc
Manas Nikte <manas.nikte@gmail.com>
2023-09-11 22:23:22 +0530

    Tenth Commit

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { Link, Form, redirect, useNavigation, useActionData } from 'react-router-dom';
+import { Link, Form, redirect } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
 import { FormRow, Logo } from '../components';
 import customFetch from '../utils/customFetch';
@@ -24,9 +24,6 @@ export const action = async ({ request }) => {
 };
 
 const Login = () => {
-  const errors = useActionData();
-  // const navigation = useNavigation();
-  const isSubmitting = navigation.state === 'submitting';
   return (
     <Wrapper>
       <Form method='post' className='form'>
@@ -36,9 +33,7 @@ const Login = () => {
         <p></p>
         <FormRow type='email' name='email' defaultValue='john@gmail.com' />
         <FormRow type='password' name='password' defaultValue='secret123' />
-        <button type='submit' className='btn btn-block' disabled={isSubmitting}>
-          {isSubmitting ? 'submitting...' : 'submit'}
-        </button>
+        <SubmitBtn />
         <button type='button' className='btn btn-block'>
           explore the app
         </button>

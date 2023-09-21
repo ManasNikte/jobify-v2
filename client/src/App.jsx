b72d16a4392deafe1514ff6968351082e99187e9
Manas Nikte <manas.nikte@gmail.com>
2023-09-21 07:53:40 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -94,7 +94,7 @@ const router = createBrowserRouter([
           },
           { 
             path: 'delete-job/:id', 
-            action: deleteJobAction,
+            action: deleteJobAction(queryClient),
           },
         ],
         },
